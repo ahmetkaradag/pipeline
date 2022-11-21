670f67d4a803f24b7bd487f95a1b9a3c814e7044
ahmetkaradag <ahmet@backbase.com>
2022-11-21 15:24:50 +0100

    #1 add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./Header";
 import Container from "./Container";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Container/>
+            <Footer/>
         </div>
     );
 }
