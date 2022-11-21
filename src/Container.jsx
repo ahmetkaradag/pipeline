d14e6b1898c0ad857df1b6936c51fa573169ac31
ahmetkaradag <ahmet@backbase.com>
2022-11-21 14:26:33 +0100

    first commit

---
+++ src/Container.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Container = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ipsam necessitatibus quaerat
+                reprehenderit
+                voluptate? Adipisci, autem ea ex impedit, minima neque nostrum porro quae quia ratione recusandae
+                repudiandae soluta voluptate!</p>
+        </div>
+    );
+
+};
+
+
+export default Container;
--- src/Header.test.js
+++ src/Header.test.js
@@ -1,8 +1,8 @@
 import {render, screen} from '@testing-library/react';
-import App from './App';
+import Header from './Header';
 
 test('renders learn react link', () => {
-    render(<App/>);
+    render(<Header/>);
     const header = screen.getByText(/Header/i);
     expect(header).toBeInTheDocument();
 });
